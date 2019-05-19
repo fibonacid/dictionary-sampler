5d5b3d4070274515ae8bd7de616ee90b1f4e566e
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-19 17:17:38 +0200

    Add console log to track actions

--- code/reducers/wordsReducer.js
+++ code/reducers/wordsReducer.js
@@ -5,11 +5,13 @@ export const words = (state={}, action) => {
    switch(action.type) {
 
       case types.GET_WORD:
+         console.log(types.GET_WORD)
          return {
             ...state,
          }
 
       case types.GET_WORD_SUCCESS:
+         console.log(types.GET_WORD_SUCCESS)
          return {
             ...state,
             payload: action.payload,
@@ -17,11 +19,13 @@ export const words = (state={}, action) => {
          }
 
       case types.GET_WORD_ERROR:
+         console.log(types.GET_WORD_ERROR)
          return {
             ...state,
-            payload:action.payload,
+            payload: action.payload,
             error: action.error
          }
+
       default:
          return state
    }
