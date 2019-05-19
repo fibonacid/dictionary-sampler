98eaa5cfb44176720013ec7bcfae69a0b1b6e15e
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-19 14:31:39 +0200

    - Add rootReducer import - Remove composeWithDevTools

--- code/store.js
+++ code/store.js
@@ -1,12 +1,13 @@
 import {createStore, applyMiddleware} from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './sagas/rootSaga'
+import {rootReducer} from './reducers/rootReducer'
 
 const sagaMiddleware = createSagaMiddleware()
 
 export const store = createStore(
    rootReducer,
-   composeWithDevTools(applyMiddleware(sagaMiddleware))
+   applyMiddleware(sagaMiddleware)
 )
 
 sagaMiddleware.run(rootSaga)
