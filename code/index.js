a07ce3a89e973099f55440e6bc73160619fb79c3
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-19 17:17:56 +0200

    Add handler for state changes

--- code/index.js
+++ code/index.js
@@ -1,4 +1,11 @@
 import {store} from './store';
 import {getWordAction} from './actions/getWordAction'
 
+const handleStateChange = () => {
+   console.log(store.getState())
+}
+
+store.subscribe(handleStateChange);
+
 store.dispatch(getWordAction('hello'))
+
