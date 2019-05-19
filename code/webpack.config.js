b4aaa15fce5f1907bd68c714c1ea52f28adf03a3
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-19 14:11:48 +0200

    Add webpack config

---
+++ code/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+   entry: './index.js',
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+   },
+   module: {
+      rules: [
+         {
+            test: /.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['es2015']
+            }
+         }
+      ]
+   },
+   node: {
+      fs: 'empty' // https://github.com/webpack-contrib/css-loader/issues/447
+   }
+};
