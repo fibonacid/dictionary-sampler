dc95918d754933d1c5225b88ce372794a58cfc07
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-20 23:05:40 +0200

    Log state changes in blue

--- code/index.js
+++ code/index.js
@@ -1,15 +1,15 @@
 import {store} from './store';
 import {getWordAction} from './actions/getWordAction'
+const clc = require('cli-color')
+
 
 const handleStateChange = () => {
-
+   let state = store.getState();
+   console.log(clc.blue(JSON.stringify(state, null, 3)))
 }
 
 store.subscribe(handleStateChange);
 
 store.dispatch(getWordAction('hello'));
 
-setTimeout(() => {
-   store.dispatch(getWordAction('goodbye'));
-}, 3000);
 
