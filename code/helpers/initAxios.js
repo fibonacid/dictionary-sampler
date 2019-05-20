85232695eaaf03bbf97fcd419c00230a75755509
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-20 22:07:56 +0200

    Add dotenv webpack plugin

--- code/helpers/initAxios.js
+++ code/helpers/initAxios.js
@@ -1,6 +1,8 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+console.log(process.env.OXFORD_APP_KEY)
+
 export function initAxios(axios) {
    axios.defaults.headers.common['app_id'] = process.env.OXFORD_APP_ID;
    axios.defaults.headers.common['app_key'] = process.env.OXFORD_APP_KEY;
--- code/webpack.config.js
+++ code/webpack.config.js
@@ -1,11 +1,14 @@
 var path = require('path');
 var webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
    entry: ['babel-polyfill', './index.js'],
    output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js'
    },
+   mode: 'development',
    // https://github.com/axios/axios/issues/456#issuecomment-285287911
    target: 'node',
    module: {
@@ -20,6 +23,9 @@ module.exports = {
          }
       ]
    },
+   plugins: [
+      new Dotenv()
+   ],
    node: {
       fs: 'empty' // https://github.com/webpack-contrib/css-loader/issues/447
    }
