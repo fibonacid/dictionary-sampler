2334c43bc9e560ac9b74834bc76cb48325617e3d
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-20 22:40:30 +0200

    Remove dotenv

--- code/helpers/initAxios.js
+++ code/helpers/initAxios.js
@@ -1,6 +1,3 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
 export function initAxios(axios) {
    axios.defaults.headers.common['app_id'] = process.env.OXFORD_APP_ID;
    axios.defaults.headers.common['app_key'] = process.env.OXFORD_APP_KEY;
