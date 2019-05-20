68d4d4ffa0f4204fc1b3d93588771bd41ca8626f
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-20 22:41:50 +0200

    Add logger middleware

--- code/store.js
+++ code/store.js
@@ -3,11 +3,23 @@ import createSagaMiddleware from 'redux-saga'
 import rootSaga from './sagas/rootSaga'
 import {rootReducer} from './reducers/rootReducer'
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
+
+const clc = require('cli-color');
+
+function logger({getState}) {
+   return next => action => {
+      console.log(clc.blue(
+`dispatching action => ${action.type} 
+${JSON.stringify(getState(), null, 3)}
+`))
+      return next(action)
+   }
+}
 
 export const store = createStore(
    rootReducer,
-   applyMiddleware(sagaMiddleware)
+   applyMiddleware(logger, sagaMiddleware)
 )
 
 sagaMiddleware.run(rootSaga)
