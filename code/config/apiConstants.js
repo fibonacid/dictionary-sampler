9a8f09fba6a2abff1930b252b3ca27544e8ad2f4
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-24 12:42:23 +0200

    Refactor OXFORD API constants

---
+++ code/config/apiConstants.js
@@ -0,0 +1,17 @@
+export const OXFORD_API = {
+   BASE_URL: "https://od-api.oxforddictionaries.com/v2",
+   FILTERS: {
+      DEFINITIONS: "definitions",
+      REGIONS: "regions",
+      SYNONYMS: "synonyms",
+      INFLECTIONS: "inflections",
+      LEXICAL_CATEGORY: "lexicalCategory",
+      GRAMMATICAL_FEATURE: "grammaticalFeatures",
+      EXAMPLES: "examples",
+      PRONUNCIATIONS: "pronunciations",
+      DOMAINS: "domains",
+      REGISTERS: "registers",
+      TRANSLATIONS: "translations",
+      ANTONYMS: "antonyms"
+   }
+}
--- code/index.js
+++ code/index.js
@@ -1,6 +1,6 @@
 import {store} from './store';
 import {getWordAction} from './actions/getWordAction'
-import oxford from './helpers/apiConstants'
+import {OXFORD_API} from './config/apiConstants'
 
 const handleStateChange = () => {
    let state = store.getState();
@@ -9,8 +9,11 @@ const handleStateChange = () => {
 store.subscribe(handleStateChange);
 
 const params = {
-   filters: [oxford.filters.pronunciations]
-}
+   filters: [
+      OXFORD_API.FILTERS.PRONUNCIATIONS,
+      OXFORD_API.FILTERS.EXAMPLES
+   ]
+};
 
 store.dispatch(getWordAction('hello', params));
 
