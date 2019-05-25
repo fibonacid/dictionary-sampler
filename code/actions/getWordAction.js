e59e6fdf0db9271072c5f6c5bf807d03001d8dca
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-25 13:54:07 +0200

    Update default params for getWordAction

--- code/actions/getWordAction.js
+++ code/actions/getWordAction.js
@@ -1,7 +1,12 @@
 import {types} from './actionTypes'
+import {OXFORD_API} from "../config/apiConstants";
 
 const defaultParams = {
-   lang: "en-us"
+   lang: "en-us",
+   filters: [
+      OXFORD_API.FILTERS.PRONUNCIATIONS,
+      OXFORD_API.FILTERS.EXAMPLES
+   ]
 }
 
 export function getWordAction(word, params={}) {
--- code/index.js
+++ code/index.js
@@ -1,6 +1,5 @@
 import {store} from './store';
 import {getWordAction} from './actions/getWordAction'
-import {OXFORD_API} from './config/apiConstants'
 
 const handleStateChange = () => {
    let state = store.getState();
@@ -8,13 +7,6 @@ const handleStateChange = () => {
 
 store.subscribe(handleStateChange);
 
-const params = {
-   filters: [
-      OXFORD_API.FILTERS.PRONUNCIATIONS,
-      OXFORD_API.FILTERS.EXAMPLES
-   ]
-};
-
-store.dispatch(getWordAction('hello', params));
+store.dispatch(getWordAction('hello'));
 
 
