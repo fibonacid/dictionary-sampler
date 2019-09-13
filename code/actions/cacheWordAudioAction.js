7905b8dd03aa74d80f38ffdc415d802fcefdddeb
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-09-13 18:31:50 +0200

    CHANGED payload: use entire word object instead of just the id

--- code/actions/cacheWordAudioAction.js
+++ code/actions/cacheWordAudioAction.js
@@ -1,6 +1,6 @@
 import {types} from './actionTypes'
 
-export const cacheWordAudioAction = word_id => ({
+export const cacheWordAudioAction = word => ({
    type: types.CACHE_WORD_AUDIO,
-   payload: { word_id }
+   payload: word
 });
