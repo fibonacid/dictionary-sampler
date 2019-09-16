5d5744e97c00b5ff3c878afe4344c28a96420100
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-16 19:05:05 +0200

    ADDED saga to download audio (not finished)

---
+++ code/sagas/DownloadAudioSaga.js
@@ -0,0 +1,42 @@
+import { types } from '../actions/actionTypes';
+import { uniqueFileName } from 'unique-filename'
+import {
+    takeLatest,
+    call,
+    put,
+    cancel,
+    take
+} from 'redux-saga/effects'
+
+export function* downloadAudioFileWatcher(action) {
+    const saga = yield takeLatest(types.DOWNLOAD_AUDIO_FILE, downloadAudioFileSaga);
+    // Listen if any there is any failure
+    const failure = yield take(types.DOWNLOAD_AUDIO_FILE_ERROR);
+    // cancel saga if a failure as happened
+    yield cancel(saga);
+}
+
+export function* downloadAudioFileSaga(action) {
+    try {
+        const payload = yield call(downloadAudio, action.payload);
+
+    }
+    catch(error) {
+        yield put({
+            type: types.DOWNLOAD_AUDIO_FILE_ERROR,
+            error: error.message
+        })
+    }
+}
+
+function downloadAudio(url) {
+    const path = generateCacheFile();
+    console.log(path);
+}
+
+function generateCacheFile() {
+    const dir = `${global.appRoot}/cache`;
+    let filename = uniqueFileName(dir);
+    return filename + ".mp3";
+};
+
