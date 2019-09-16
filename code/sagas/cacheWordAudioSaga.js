cd499a7030321adf0be7b03a878a43d0ef87353f
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-16 10:48:03 +0200

    CHANGED use this saga to gather resources for caching. Download should happen inside an independent saga.

--- code/sagas/cacheWordAudioSaga.js
+++ code/sagas/cacheWordAudioSaga.js
@@ -1,50 +1,30 @@
 import {types} from '../actions/actionTypes'
-import { takeLatest, call, put } from 'redux-saga/effects'
-import { download } from '../lib/helpers/download'
-import uniqueFilename from 'unique-filename';
+import { takeLatest, call, put, all } from 'redux-saga/effects'
 
 export function* cacheWordAudioWatcher() {
    const saga = yield takeLatest(types.CACHE_WORD_AUDIO, cacheWordAudioSaga)
 }
 
 export function* cacheWordAudioSaga(action) {
-   const payload = yield call(downloadAudio, action.payload);
-}
-
-const path = require('path');
-const tmp = require('tmp');
-
-function downloadAudio(payload) {
-   const { pronunciations } = payload;
-   const dir = `${global.appRoot}/cache`;
-   const cacheDir = path.resolve(dir, "");
-   console.log(`cache directory: ${cacheDir}`);
-   if (pronunciations) {
-      Object.keys(payload.pronunciations).forEach(function(key) {
-         const filename = uniqueFilename(cacheDir) + ".mp3";
-         let { url } = pronunciations[key];
-         console.log(`file for ${key}: ${filename}`);
-         download(url, filename)
-            .then(() => { console.log(`download finished`) })
-            .catch(error => { console.log(error) })
-      });
+   const { urls } = yield call(getUrls, action.payload);
+   try {
+      if (typeof urls !== "undefined") {
+         yield put({ type: types.CACHE_WORD_AUDIO_SUCCESS, payload: urls})
+      }
+   }
+   catch(error) {
+      yield put({ type: types.CACHE_WORD_AUDIO_ERROR, error})
    }
 }
 
-/*
- const options = {
-            prefix: filename,
-            postfix: `.mp3`,
-            dir: `${global.appRoot}/cache`
-         };
- tmp.file(options, (err, path) => {
-            if (err) throw err;
-            download(url, filename)
-               .then(function () {
-                  console.log(`${key}: download complete`);
-               })
-               .catch( error => {
-                  console.log(error);
-               })
-         });
- */
+function getUrls(payload) {
+   const { pronunciations } = payload;
+   let urls = [];
+   if (pronunciations) {
+      Object.keys(payload.pronunciations).forEach(function(key) {
+         let { url } = pronunciations[key];
+         urls.push(url);
+      });
+   }
+   return urls;
+}
