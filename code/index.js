85fdd0310656a777623e3dcca3a7e7106cee8633
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-18 21:56:23 +0200

    ADDED search_text event handler

--- code/index.js
+++ code/index.js
@@ -2,6 +2,7 @@ import {store} from './store';
 import {addWordAction} from './actions/addWordAction'
 import { maxApi } from "./lib/config/maxApi";
 import {searchWordAction} from "./actions/searchWordAction";
+import {searchWordsFromTextAction} from "./actions/searchWordsFromTextAction";
 
 var path = require('path');
 global.appRoot = path.resolve(process.cwd(), "..");
@@ -19,6 +20,9 @@ store.subscribe(handleStateChange);
 //store.dispatch(getWordAction('hello'));
 
 const handlers = {
+   search_text: text => {
+      store.dispatch(searchWordsFromTextAction(text))
+   },
    search_word: word => {
        store.dispatch(searchWordAction(word));
    },
