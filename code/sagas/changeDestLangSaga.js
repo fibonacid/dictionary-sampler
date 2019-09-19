9d2ac6f5b11d454e09fa6c007a25fde96718bc03
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-19 12:55:50 +0200

    CHANGED success payload's form

--- code/sagas/changeDestLangSaga.js
+++ code/sagas/changeDestLangSaga.js
@@ -1,10 +1,28 @@
 import {types} from '../actions/actionTypes'
-import { takeLatest } from 'redux-saga/effects'
+import {put, takeLatest} from 'redux-saga/effects'
+import {isValidLang} from "../lib/helpers/common";
+import {OXFORD_API} from "../lib/config/apiConstants";
 
 export function* changeDestLangWatcher() {
     yield takeLatest(types.CHANGE_DEST_LANG, changeDestLangSaga);
 }
 
 export function* changeDestLangSaga(action) {
-
+    try {
+        const lang = action.payload;
+        if (isValidLang(lang)) {
+            yield put({
+                type: types.CHANGE_DEST_LANG_SUCCESS,
+                payload: OXFORD_API.LANGUAGES[lang]
+            })
+        } else {
+            throw new Error(`Unknown language: ${lang}`)
+        }
+    }
+    catch(error) {
+        yield put({
+            type: types.CHANGE_DEST_LANG_ERROR,
+            error
+        })
+    }
 }
--- code/sagas/changeSrcLangSaga.js
+++ code/sagas/changeSrcLangSaga.js
@@ -1,10 +1,28 @@
 import {types} from '../actions/actionTypes'
-import { takeLatest } from 'redux-saga/effects'
+import { takeLatest, put } from 'redux-saga/effects'
+import {isValidLang} from "../lib/helpers/common";
+import {OXFORD_API} from "../lib/config/apiConstants";
 
 export function* changeSrcLangWatcher() {
     yield takeLatest(types.CHANGE_SRC_LANG, changeSrcLangSaga);
 }
 
 export function* changeSrcLangSaga(action) {
-
+    try {
+        const lang = action.payload;
+        if (isValidLang(lang)) {
+            yield put({
+                type: types.CHANGE_SRC_LANG_SUCCESS,
+                payload: OXFORD_API.LANGUAGES[lang]
+            })
+        } else {
+            throw new Error(`Unknown language: ${lang}`)
+        }
+    }
+    catch(error) {
+        yield put({
+            type: types.CHANGE_SRC_LANG_ERROR,
+            error
+        })
+    }
 }
