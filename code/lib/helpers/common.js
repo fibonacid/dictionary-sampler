32a35f1fabdadd72dbe22e734c9c6a45af7fe43e
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-19 12:53:53 +0200

    CHANGED function `isValidLang`

--- code/lib/helpers/common.js
+++ code/lib/helpers/common.js
@@ -1,7 +1,17 @@
+import {OXFORD_API} from "../config/apiConstants";
+import _ from 'lodash';
+
 export const selectWord = (state, word) => {
     const { data } = state.words;
     if (!data) {
         return;
     }
     return data[word];
+};
+
+export const isValidLang = lang => {
+  if (typeof lang !== "string") {
+    throw new Error("lang must be a string");
+  }
+  return _.has(OXFORD_API.LANGUAGES, lang);
 };
