ce330ce546ea3f02256a95cc630db33f8cf4092d
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-20 15:14:38 +0200

    CHANGED clean log

--- code/sagas/actionLogSaga.js
+++ code/sagas/actionLogSaga.js
@@ -5,8 +5,7 @@ import {maxApi} from "../lib/config/maxApi";
 export function* actionLogWatcher() {
     yield takeEvery('*', function* logger(action) {
         const postLevel = getPostLevelFromActionType(action.type);
-        const message =
-            `${action.type} => ${action.payload}`;
+        const message = action.type;
         maxApi.post(message, postLevel);
     })
 };
