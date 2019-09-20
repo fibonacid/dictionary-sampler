8f4cbf680d00e9e3eb7c62fe5fe18dbadf5b7857
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-20 12:13:31 +0200

    REMOVED logger

--- code/store.js
+++ code/store.js
@@ -2,23 +2,12 @@ import {createStore, applyMiddleware} from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './sagas/rootSaga'
 import {rootReducer} from './reducers/rootReducer'
-import { maxApi } from "./lib/config/maxApi";
 
 const sagaMiddleware = createSagaMiddleware();
 
-function logAction() {
-   return next => action => {
-      const message =
-          `${action.type} => \t${action.payload || action.error}`;
-      console.log(message);
-      maxApi.post(message);
-      return next(action)
-   }
-}
-
 export const store = createStore(
    rootReducer,
-   applyMiddleware(logAction, sagaMiddleware)
+   applyMiddleware(sagaMiddleware)
 );
 
 sagaMiddleware.run(rootSaga);
