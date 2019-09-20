316851edc4741668e4d6628af4116d9d488afe81
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-20 17:33:13 +0200

    ADDED status keys

--- code/lib/config/constants.js
+++ code/lib/config/constants.js
@@ -1,6 +1,8 @@
 export const SEARCH_STATUS = {
+  FAILED: "failed",
   QUEUED: "queued",
   LOADING: "loading",
   LOADED: "loaded",
-  AVAILABLE: "available"
+  AVAILABLE: "available",
+  UNAVAILABLE: "unavailable"
 };
