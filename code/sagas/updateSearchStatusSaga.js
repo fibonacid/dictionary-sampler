b56b020953a3fd37c79ba27fd02d7222a43a4d44
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-21 18:59:35 +0200

    CHANGED: use takeEvery instead of takeLatest (saga effects)

--- code/sagas/updateSearchStatusSaga.js
+++ code/sagas/updateSearchStatusSaga.js
@@ -1,11 +1,12 @@
-import {takeLatest} from "redux-saga/effects";
+import {takeEvery} from "redux-saga/effects";
 import {types} from "../actions/actionTypes";
 
 export function* updateSearchStatusWatcher() {
-    yield takeLatest(types.UPDATE_SEARCH_STATUS, updateSearchStatusSaga)
+    yield takeEvery(types.UPDATE_SEARCH_STATUS, updateSearchStatusSaga)
 }
 
 export function* updateSearchStatusSaga(action) {
     const { id, status } = action.payload;
     console.log(`[UPDATE] ${JSON.stringify(action.payload)}`);
+
 }
