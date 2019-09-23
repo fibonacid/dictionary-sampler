63d016eb07bf5bcefd38dfb0597aad433c8517bf
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-23 20:27:39 +0200

    REMOVED sagas

--- code/sagas/rootSaga.js
+++ code/sagas/rootSaga.js
@@ -1,40 +1,10 @@
 import {all} from 'redux-saga/effects'
 import {searchWordWatcher} from "./searchWordSaga";
-import {addWordWatcher} from "./addWordSaga";
-import {cacheWordAudioWatcher} from "./cacheWordAudioSaga";
-import {maxObjectOutputWatcher} from "./maxObjectOutputSaga";
-import {searchWordsFromTextWatcher} from "./searchWordsFromTextSaga";
-import {maxConsolePrintWatcher} from "./maxConsolePrintSaga";
-import {logStoreWatcher} from "./logStore";
-import {stopAllWatcher} from "./stopAllSaga";
-import {clearCacheWatcher} from "./clearCacheSaga";
-import {changeSrcLangWatcher} from "./changeSrcLangSaga";
-import {changeDestLangWatcher} from "./changeDestLangSaga";
-import {resetAppWatcher} from "./resetAppSaga";
-import {actionLogWatcher} from "./actionLogSaga";
-import {updateSearchStatusWatcher} from "./updateSearchStatusSaga";
-import {addSearchWatcher} from "./addSearchSaga";
-import {searchWordsFromWebpageWatcher} from "./searchWordsFromWebPageSaga";
 
 // notice how we now only export the rootSaga
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
    yield all([
-      actionLogWatcher(),
-      addWordWatcher(),
-      addSearchWatcher(),
-      cacheWordAudioWatcher(),
-      changeSrcLangWatcher(),
-      changeDestLangWatcher(),
-      clearCacheWatcher(),
-      logStoreWatcher(),
-      maxObjectOutputWatcher(),
-      maxConsolePrintWatcher(),
-      resetAppWatcher(),
-      searchWordsFromTextWatcher(),
-      searchWordsFromWebpageWatcher(),
       searchWordWatcher(),
-      stopAllWatcher(),
-      updateSearchStatusWatcher()
    ])
 }
--- code/sagas/searchWordSaga.js
+++ code/sagas/searchWordSaga.js
@@ -1,67 +1,7 @@
-import {types} from '../actions/actionTypes'
-import { takeEvery, select, put, take, actionChannel, fork, delay} from 'redux-saga/effects'
-import {addWordAction} from "../actions/addWordAction";
-import {maxObjectOutputAction} from "../actions/maxObjectOutputAction";
-import {selectSearch, selectWord} from "../lib/helpers/common";
-import {addSearchAction} from "../actions/addSearchAction";
-import {updateSearchStatusAction} from "../actions/updateSearchStatusAction";
-import {SEARCH_STATUS} from "../lib/config/constants";
-import {cacheWordAudioAction} from "../actions/cacheWordAudioAction";
-
-const MINIMUM_WAIT = 50; // ms
-
 export function* searchWordWatcher() {
-    // 1- Create a channel for request actions
-    /*const requestChan = yield actionChannel(types.SEARCH_WORD);
-    while (true) {
-        // 2- take from the channel
-        const action = yield take(requestChan);
-        // 3- Wait some millis
-        yield delay(MINIMUM_WAIT);
-        // 4- Note that we're using a blocking call
-        yield call(searchWordSaga, action);
-    }*/
-    yield takeEvery(types.SEARCH_WORD, searchWordSaga)
+
 }
 
 export function* searchWordSaga(action) {
-    try {
 
-        const word = yield select(selectWord, action.payload);
-        if (typeof word !== "undefined") {
-            yield put({
-                type: types.SEARCH_WORD_FOUND,
-                payload: word
-            });
-            if (word.audio_file) {
-                yield put(maxObjectOutputAction(word.audio_file));
-            } else {
-                yield put(cacheWordAudioAction(word))
-            }
-        } else {
-            yield put({
-                type: types.SEARCH_WORD_NOT_FOUND,
-                payload: action.payload
-            });
-
-            const search_id = yield put(addSearchAction(action.payload));
-
-            const { src_lang } = yield select(state => state.base);
-            yield put(addWordAction(action.payload, {
-                lang: src_lang,
-                search_id
-            }));
-        }
-     }
-    catch(error) {
-        yield put({
-            type: types.SEARCH_WORD_ERROR,
-            error: error.message
-        });
-
-        const search_id = select(selectSearch, action.payload);
-        if (search_id) {
-            yield put(updateSearchStatusAction(search_id, SEARCH_STATUS.FAILED))
-        }
-    }
 }
