77a1daf6e97c9cd44c6d16a2e2169a8c051fdacf
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-25 00:05:33 +0200

    ADDED helpers for particular types of max outputs

--- code/lib/config/maxApi.js
+++ code/lib/config/maxApi.js
@@ -1,1 +1,21 @@
 export const maxApi = __non_webpack_require__("max-api");
+
+export function maxMessageOut(message) {
+    return maxApi.outlet(message);
+}
+
+export function maxSetDictionary(dictName, data) {
+    return maxApi.setDict(dictName, data);
+}
+
+export function maxConsoleOut(message) {
+    return maxApi.post(message);
+}
+
+export function maxPolyTildeMessage(voiceNumber, message) {
+    return maxApi.outlet(`target ${voiceNumber}, ${message}`);
+}
+
+export function maxSendRefresh() {
+    return maxApi.outlet("refresh")
+}
