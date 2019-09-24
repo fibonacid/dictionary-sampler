ead1bce579a52a80bdaf87dfc7a6a62540c11985
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-25 00:05:58 +0200

    ADDED cleanId helper

--- code/lib/helpers/common.js
+++ code/lib/helpers/common.js
@@ -1,21 +1,5 @@
-import {OXFORD_API} from "../config/apiConstants";
-import _ from 'lodash';
-
-export const selectWord = (state, word) => {
-    const { data } = state.words;
-    if (!data) {
-        return;
-    }
-    return data[word];
-};
-
-export const selectSearch = (state, search_id) => {
-    return state.searches[search_id];
-};
-
-export const isValidLang = lang => {
-  if (typeof lang !== "string") {
-    throw new Error("lang must be a string");
-  }
-  return _.has(OXFORD_API.LANGUAGES, lang);
+export const cleanID = id => {
+    id = id.replace(/\W/g, '');
+    id = id.toLowerCase();
+    return id;
 };
