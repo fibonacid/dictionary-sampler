16c4e70d6eba13db85846c8b8cdd26d1f7ddd7fc
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-24 15:02:50 +0200

    ADDED message handlers

--- code/index.js
+++ code/index.js
@@ -1,14 +1,30 @@
 import {store} from './store';
 import { maxApi } from "./lib/config/maxApi";
 import {searchWordAction} from "./actions/searchWordAction";
+import {initializeStoreAction} from "./actions/initializeStoreAction";
+import {clearCacheAction} from "./actions/clearCacheAction";
+import {logStoreAction} from "./actions/logStoreAction";
 
 var path = require('path');
 global.appRoot = path.resolve(process.cwd(), "..");
 
 const handlers = {
-    search_word: word => {
-        store.dispatch(searchWordAction(word))
-    }
+    initialize_store: function() {
+        store.dispatch(initializeStoreAction());
+    },
+    log_store: function() {
+        store.dispatch(logStoreAction())
+    },
+    clear_cache: function() {
+        store.dispatch(clearCacheAction())
+    },
+    reset_app: function() {
+        store.dispatch(clearCacheAction());
+        store.dispatch(initializeStoreAction());
+    },
+    search_word: function(word) {
+        store.dispatch(searchWordAction(word));
+    },
 };
 
 /** */
