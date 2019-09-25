5752f723a881e9cbca8fc971e1d729ae20b7f962
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-25 23:04:13 +0200

    ADDED plugin: transform-class-properties

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -17,9 +17,6 @@ module.exports = {
             test: /.js$/,
             exclude: /node_modules/,
             loader: 'babel-loader',
-            query: {
-               presets: ['es2015']
-            }
          }
       ]
    },
