77e6bf3c08d17a14b2083b414e256183aa3fc3a1
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-26 17:10:17 +0200

    ADDED response digestion function

--- code/sagas/fetchWordSaga.js
+++ code/sagas/fetchWordSaga.js
@@ -1,6 +1,7 @@
 import {takeEvery, call} from "redux-saga/effects";
 import {types} from "../actions/actionTypes";
 import {OxfordApi} from "../lib/helpers/OxfordApi";
+import _ from 'lodash'
 
 export function* fetchWordWatcher() {
     yield takeEvery(types.FETCH_WORD, fetchWordSaga)
@@ -9,7 +10,30 @@ export function* fetchWordWatcher() {
 export function* fetchWordSaga(action) {
     const word = action.payload;
     const response = yield call(OxfordApi.searchWord, word);
-
-    console.log(response.status);
+    const fetchedWord = digestResponse(response);
+    console.log(fetchedWord);
 }
 
+function digestResponse(response) {
+    const { data } = response;
+    if (!data ||
+        !data.results
+        || data.results.length === 0) {
+        return undefined;
+    }
+    let outputObject = {};
+    // Retrieve first one
+    const result = data.results[0];
+    const { id, lexicalEntries } = result;
+    // Add id property to the word object
+    outputObject.id = id;
+    // Get first lexical entry with property "pronunciations"
+    const entry = _.find(lexicalEntries, e => e.pronunciations);
+    if (entry) {
+        const { audioFile } = _.find(entry.pronunciations, p => {
+            return typeof p.audioFile != "undefined"
+        });
+        outputObject.audioFileUrl = audioFile;
+    }
+    return outputObject;
+}
