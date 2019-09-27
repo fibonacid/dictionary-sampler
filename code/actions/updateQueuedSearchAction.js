84aa90d16a19344b77ef60bd971065440518cdfa
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-27 10:41:05 +0200

    CHANGED payload id prop name

--- code/actions/updateQueuedSearchAction.js
+++ code/actions/updateQueuedSearchAction.js
@@ -3,7 +3,7 @@ import {types} from "./actionTypes";
 export const updateQueuedSearchAction = (searchId, status) => ({
     type: types.UPDATE_QUEUED_SEARCH,
     payload: {
-        searchId,
+        id: searchId,
         status
     }
 });
