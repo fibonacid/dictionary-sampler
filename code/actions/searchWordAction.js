d282147a95628bb23d2811a3d8db33485a9aa017
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-27 15:06:54 +0200

    ADDED actions

--- code/actions/searchWordAction.js
+++ code/actions/searchWordAction.js
@@ -4,3 +4,11 @@ export const searchWordAction = word => ({
    type: types.SEARCH_WORD,
    payload: word
 });
+
+export const searchWordSuccessAction = searchId => ({
+   type: types.SEARCH_WORD_SUCCESS
+});
+
+export const searchWordTimeoutAction = searchId => ({
+   type: types.SEARCH_WORD_TIMEOUT
+});
