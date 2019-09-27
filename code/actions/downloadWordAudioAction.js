bfd448eb0b8dd013d5302771b083f2d2149e6d18
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-27 11:23:07 +0200

    ADDED success and fail actions

--- code/actions/downloadWordAudioAction.js
+++ code/actions/downloadWordAudioAction.js
@@ -4,3 +4,12 @@ export const downloadWordAudioAction = word => ({
     type: types.DOWNLOAD_WORD_AUDIO,
     payload: word
 });
+
+export const downloadWordAudioSuccessAction = filename => ({
+    type: types.DOWNLOAD_WORD_AUDIO_SUCCESS
+});
+
+export const downloadWordAudioErrorAction = error => ({
+    type: types.DOWNLOAD_WORD_AUDIO_SUCCESS,
+    error
+});
