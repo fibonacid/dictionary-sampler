efc94eb9e5aa5e45d08c97c1875fbb1249d718f1
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-27 12:26:02 +0200

    ADDED wait for download audio success, then output poly message with filename

--- code/sagas/searchWordSaga.js
+++ code/sagas/searchWordSaga.js
@@ -8,13 +8,16 @@ import {
     select,
     take,
     delay,
-    call
+    call,
+    race
 } from 'redux-saga/effects'
 import {
     maxPolyTildeMessage,
     maxSendRefresh,
 } from "../lib/config/maxApi";
 import {fetchWordAction} from "../actions/fetchWordAction";
+import {updateQueuedSearchAction} from "../actions/updateQueuedSearchAction";
+import {QUEUE_STATUS} from "../lib/config/constants";
 
 const searchSelector = (state, searchId) => state.queue[searchId];
 const wordSelector = (state, wordId) => {
@@ -53,13 +56,12 @@ export function* searchWordSaga(action) {
 
     // 3. Add search to queue
     yield put(addSearchToQueueAction(searchId, query));
-    const storedSearch = yield select(searchSelector, searchId);
+    let searchInstance = yield select(searchSelector, searchId);
 
     // Refresh Max dictionary
     yield call(maxSendRefresh);
-
     // Output max poly~ message "queued"
-    yield call(maxPolyTildeMessage, searchCount, storedSearch.status);
+    yield call(maxPolyTildeMessage, searchCount, searchInstance.status);
 
 /* ==============================
  * PROVIDE WORD FOR QUERY
@@ -71,6 +73,24 @@ export function* searchWordSaga(action) {
     if (!storedWord) {
         // Fetch word from API
         yield put(fetchWordAction(query));
+        // Wait for either a success or an error
+        const { success } = yield race({
+            success: take(types.DOWNLOAD_WORD_AUDIO_SUCCESS),
+            timeout: delay(5000)
+        });
+        // If success won the race
+        if (success) {
+            // Change status of search
+            const newStatus = QUEUE_STATUS.AVAILABLE;
+            yield put(updateQueuedSearchAction(searchId, newStatus));
+            // Refresh Max dictionary
+            yield call(maxSendRefresh);
+            // Output max poly~ message
+            yield call(maxPolyTildeMessage, searchCount,
+                `${newStatus} ${success.payload}`
+            );
+            //console.log(JSON.stringify(success, null, 2));
+        }
     }
     // If word has already been fetched
     else {
@@ -78,7 +98,7 @@ export function* searchWordSaga(action) {
         if (_.has(storedWord, "audioFile")) {
             // Send audioFile to max poly~ object
             yield call(maxPolyTildeMessage,
-                `${storedSearch.status} ${storedWord.audioFile}`
+                `${searchInstance.status} ${storedWord.audioFile}`
             );
         }
     }
